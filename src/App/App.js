a29192ead1ff18cf11c7e0eec31fe1e4c5a15f60
Ben McClung <aurumvalian@gmail.com>
2020-12-03 11:55:36 -0700

    Install and set up react-router-dom

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Header from '../Header/Header'
 
+import React from 'react';
+import { Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
