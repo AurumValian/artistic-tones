0a43f8ba6d36e62f571362eabd6b601cdf89f70d
Ben McClung <aurumvalian@gmail.com>
2020-12-03 12:34:12 -0700

    Add sound, harpstuff and books routes

--- src/App/App.js
+++ src/App/App.js
@@ -8,9 +8,30 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <section>
-        This website is in the process of being built!
-      </section>
+      <Route exact path="/" render={() =>
+        <section>
+          This website is in the process of being built!
+        </section>
+        }
+      />
+      <Route exact path="/harpstuff" render={() =>
+        <section>
+          This is where harpstuff goes!
+        </section>
+        }
+      />
+      <Route exact path="/books" render={() =>
+        <section>
+          This is where books go!
+        </section>
+        }
+      />
+      <Route exact path="/sound" render={() =>
+        <section>
+          This is where sound stuff goes!
+        </section>
+      }
+      />
     </div>
   );
 }
